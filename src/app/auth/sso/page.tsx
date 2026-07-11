b1ea564efab13fd3b043ae9123543f687f5cd07e
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2026-07-12 05:18:05 +1000

    refactor: move sso to app router

--- src/app/auth/sso/page.tsx
+++ src/app/auth/sso/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { getSupabaseBrowserClient } from "@/lib/supabase/client";
 
 export default function SSO() {
--- src/pages/_app.tsx
+++
@@ -1,5 +0,0 @@
-import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;
-}
